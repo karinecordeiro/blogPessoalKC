1dfe8c15498c2178afc6d9abce174bfa778915b7
Karine C <karine.cordeiro1998@hotmail.com>
2022-05-03 18:09:16 -0300

    alteração de model usuario e usuario login

--- src/main/java/org/generation/blogpessoal/model/Usuario.java
+++ src/main/java/org/generation/blogpessoal/model/Usuario.java
@@ -41,6 +41,8 @@ public class Usuario {
 	
 	private String foto;
 	
+	private String tipo;
+	
 	@OneToMany(mappedBy="usuario", cascade=CascadeType.REMOVE)
 	@JsonIgnoreProperties ("usuario")
 	private List<Postagem>postagem;
@@ -112,6 +114,14 @@ public class Usuario {
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 	
 	
